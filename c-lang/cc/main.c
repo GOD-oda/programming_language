2ce7c9d09d0fd0278c4c08bbbf727c5ce8feef01
t.oda <takahiro.tech.oda@gmail.com>
2021-01-03 04:01:14 +0900

    Update

--- c-lang/cc/main.c
+++ c-lang/cc/main.c
@@ -1,35 +1,127 @@
+#include <ctype.h>
+#include <stdarg.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int main(int argc, char **argv)
-{
+typedef enum {
+  TK_RESERVED,
+  TK_NUM,
+  TK_EOF,
+} TokenKind;
+
+typedef struct Token Token;
+
+struct Token {
+  TokenKind kind;
+  Token *next;
+  int val;
+  char *str;
+};
+
+Token *token;
+
+void err(char *fmt, ...) {
+  va_list ap;
+  va_start(ap, fmt);
+  vfprintf(stderr, fmt, ap);
+  fprintf(stderr, "\n");
+  exit(1);
+}
+
+void next() {
+  token = token->next;
+}
+
+bool check(char op) {
+  if (token->kind != TK_RESERVED || token->str[0] != op) {
+    return false;
+  }
+
+  next();
+  return true;
+}
+
+bool check_num() {
+  return token->kind == TK_NUM;
+}
+
+int val() {
+  return token->val;
+}
+
+bool at_eof() {
+  return token->kind == TK_EOF;
+}
+
+Token *new_token(TokenKind kind, Token *cur, char *str) {
+  Token *tok = calloc(1, sizeof(Token));
+  tok->kind = kind;
+  tok->str = str;
+  cur->next = tok;
+  return tok;
+}
+
+Token *tokenize(char *p) {
+  Token head;
+  head.next = NULL;
+  Token *cur = &head;
+
+  while (*p) {
+    if (isspace(*p)) {
+      p++;
+      continue;
+    }
+
+    if (*p == '+' || *p == '-') {
+      cur = new_token(TK_RESERVED, cur, p++);
+      continue;
+    }
+
+    if (isdigit(*p)) {
+      cur = new_token(TK_NUM, cur, p);
+      cur->val = strtol(p, &p, 10);
+      continue;
+    }
+
+    err("Can not tokenize");
+  }
+
+  new_token(TK_EOF, cur, p);
+  return head.next;
+}
+
+int main(int argc, char **argv) {
   if (argc != 2) {
     fprintf(stderr, "Wrong arguments");
     return 1;
   }
 
-  char *p = argv[1];
+  token = tokenize(argv[1]);
 
   printf(".intel_syntax noprefix\n");
   printf(".global main\n");
   printf("main:\n");
-  printf("  mov rax, %ld\n", strtol(p, &p, 10));
 
-  while (*p) {
-    if (*p == '+') {
-      p++;
-      printf("  add rax, %ld\n", strtol(p, &p, 10));
+  if (!check_num()) {
+    err("Not number");
+  }
+  printf("  mov rax, %d\n", val());
+  next();
+
+  while (!at_eof()) {
+    if (check('+')) {
+      printf("  add rax, %d\n", val());
+      next();
       continue;
     }
 
-    if (*p == '-') {
-      p++;
-      printf("  sub rax, %ld\n", strtol(p, &p, 10));
+    if (check('-')) {
+      printf("  sub rax, %d\n", val());
+      next();
       continue;
     }
-
-    fprintf(stderr, "Bad token: '%c'\n", *p);
-    return 1;
   }
 
   printf("  ret\n");
